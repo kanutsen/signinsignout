c93bb773891bcb8d5f6f4aa3b15c567f5a21026f
kanutsen <117545167+kanutsen@users.noreply.github.com>
2023-09-09 17:51:50 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,69 @@
 // Initialize local storage array
 let records = [];
 
+// Array of student names
+const studentNames = [
+  'Antolin, Grace',
+  'Burgett, Armani',
+  'Crawford Guzman, Desaire',
+  'Hall, Sahara',
+  'Love III, Dwayne',
+  'Olguin Aguilar, Ixayahuani',
+  'Silva, Emanuel',
+  'Agredano, Adonias',
+  'Aguilera, Marco',
+  'Ahmad, Amari',
+  'Arias, Gisselle',
+  'Branche, Mark',
+  'Campbell, La\'Teacha',
+  'Crawford, A\'niya',
+  'Cruz, Elisa',
+  'Dear, Dragon',
+  'Ebron, Denisse',
+  'Garcia, Avi',
+  'Gildersleeve, Miles',
+  'Gohlke, Winter',
+  'Hamilton, Deveicho',
+  'Jackson, Janiah',
+  'Lopez, Christopher',
+  'Martin, O\'cean',
+  'Orozco, Alejandra',
+  'Perez Gutierrez, George',
+  'Robinson, Jade',
+  'Veliz, Manik',
+  'Washington, Dajon',
+  'Bautista, Juan',
+  'Bryant JR., Michael',
+  'Chheang-SAR, Alynna',
+  'Cortez, Rylen',
+  'Cruz, Aiden',
+  'Everett, Norisha',
+  'Galeana, Orlando',
+  'Isaac, Jahdique',
+  'Martinez Garcia, Sophie',
+  'Quesada, Ruben',
+  'Spencer, Semajay',
+  'Streicker, Spencer',
+  'Talavera-Almaraz, Antonio',
+  'Young, Kamarrie',
+  'Chupin, Ashley',
+  'Collins, Danisha',
+  'Huehuetlahtoa, Akinyemi',
+  'Isaac, Malejah',
+  'Martinez Garcia, Jonathan',
+  'Powell, Aniyyah'
+];
+
+// Function to populate student names
+function populateStudentNames() {
+  const dataList = document.getElementById('studentNames');
+  studentNames.forEach(name => {
+    const option = document.createElement('option');
+    option.value = name;
+    dataList.appendChild(option);
+  });
+}
+
 // Function to record action
 function recordAction(action) {
   const studentName = document.getElementById('studentName').value;
@@ -13,3 +76,6 @@ function recordAction(action) {
 function exportData() {
   // Convert records to CSV format and download
 }
+
+// Call the function when the page loads
+window.onload = populateStudentNames;
